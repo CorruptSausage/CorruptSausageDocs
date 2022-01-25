750cd3d6ceb7370cbe30fb565d487a8219351136
Jordy Slaats <Slaats.jordy@outlook.com>
2022-01-25 14:12:27 +0100

    yeet

--- TODOWebsite/inc/basic-heads.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="./css/navbar.css" type="text/css">
-<link rel="stylesheet" href="./css/basic.css" type="text/css">
-<link rel = "icon" href ="./Resources/icon.png" type = "image/x-icon">
