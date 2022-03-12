a032f945d35f4a374d9a900f602d5f8d48b6dcde
Jordy Slaats <Slaats.jordy@icloud.com>
2022-03-12 20:16:31 +0100

    yeet

---
+++ InformationWebsite/includes/rangenHeads.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8" />    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="CorruptSausage" />
+    <link rel="stylesheet" href="./lib/bootstrap/dist/css/bootstrap.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="icon" type="image/png" href="../favicon.ico" />
+    <link rel="stylesheet" href="../css/site.css" type="text/css">
---
+++ InformationWebsite/rangen/EersteHoofdCommissaris.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("../includes/rangenHeads.php"); ?>
+    <title>Politie Tedeapolis</title>
+</head>
+
+<body>
+    <?php include("../includes/rangenNavbar.php"); ?>
+    <div class="container">
+        <main role="main" class="pb-3">
+
+        </main>
+    </div>
+
+
+    <script src="../lib/jquery/dist/jquery.min.js"></script>
+    <script src="../lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../js/site.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
