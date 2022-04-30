23761bccd279b7d4e82afb5ea04eb445b74113c0
CorruptSausage <56514707+CorruptSausage@users.noreply.github.com>
2022-04-30 15:26:32 +0200

    update

--- InformationWebsite/rangen/Aspirant.php
+++ InformationWebsite/rangen/Aspirant.php
@@ -12,9 +12,21 @@
 <body>
     <?php include("../includes/rangenNavbar.php"); ?>
     <div class="container">
-        <main role="main" class="pb-3">
-
-        </main>
+        <h1>Aspirant Informatie</h1>
+        <div class="rangen_algemeen_informatie">
+        <div class="rangen_algemeen_left_container">
+                <img src="../resources/Aspirant_rang.png" alt="Aspirant" style="width: 100%;">
+            </div>
+            <div class="rangen_algemeen_left_container">
+                <p>De Aspirant, dit is de eerste functie die je krijgt binnen het Politiekorps van Tedeapolis. Deze functie staat tegenwoordig bekend als de studentenrang. Je hebt precies dezelfde bevoegdheden als een normale politieagent, alleen zitten er een aantal verschillen tussen.
+                    <br>
+                    <br>
+                    Je draagt bijvoorbeeld geen vuurwapen, dus je bent niet bevoegd om alleen aanrijdend te gaan naar meldingen die dreigend of gevaarlijk zouden kunnen zijn. Als aspirant volg je een mentortraject, hierin leer je de basisvaardigheden van het functioneren als politiemedewerker. Vanaf Aspirant heb je de mogelijkheid tot het behalen van je Wetskennistoets waaruit je veel voordelen kan hebben tijdens jou diensten en voortgang.
+                    <br>
+                    <br>
+                    Om gepromoveerd te worden, dien je het mentortraject positief af te ronden. Meer informatie hierover kun je verkrijgen bij je mentor. Om kans te maken op de promotie van Aspirant naar Surveillant zit geen minimale promotietijd gekoppeld.</p>
+            </div>
+        </div>
     </div>
 
 
