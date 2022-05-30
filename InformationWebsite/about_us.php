3066cb2aeef591ae07e92fe26263bc133cd6e5e6
Jordy Slaats <Slaats.jordy@icloud.com>
2022-05-30 16:03:06 +0200

    update

--- InformationWebsite/about_us.php
+++ InformationWebsite/about_us.php
@@ -16,20 +16,115 @@
         <h1>Over ons</h1>
     </div>
 
-    <div class="container">
-        <h1>Welkom op de Politie website van Tedeapolis!</h1>
-        <p>
-            Op deze website kun je informatie vinden betreft ons korps
-        </p>
+    <div class="line_black">
     </div>
 
+    <div class="beschermen_begrenzen_bekrachtigen">
+        <div class="beschermen_container">
+            <img src="./resources/shield-fill (1).svg" alt="Beschermen" style="width: 50%;">
+            <h2>Beschermen</h2>
+            <p>Waar anderen een stap terug doen, stappen politiemedewerkers naar voren.</p>
+        </div>
+        <div class="begrenzen_container">
+            <img src="./resources/speedometer.svg" alt="Begrenzen" style="width: 50%;">
+            <h2>Begrenzen</h2>
+            <p>Aan onwettig gedrag, ongeacht of het kleine overtredingen of zware misdrijven zijn, stelt de politie paal en perk.</p>
+        </div>
+        <div class="bekrachtigen_container">
+            <img src="./resources/lightning.svg" alt="Bekrachtigen" style="width: 50%;">
+            <h2>Bekrachtigen</h2>
+            <p>Ondersteuning van gewenst gedrag en het creëren van structurele samenwerkingsverbanden die de veiligheid bevorderen.</p>
+        </div>
+
+    </div>
+
+    <div class="line_black">
+    </div>
+
+    <div class="container">
+        <h2 class="title_blue">Waar staat de politie voor?</h2>
+        <ul>
+            <li>Veiligheidszorg. Iedereen binnen Nederland moet zich veilig kunnen voelen.</li>
+            <li>Criminaliteit voorkomen en bestrijden.</li>
+            <li>De openbare orde bewaken. De openbare orde is de rust en de orde op straat en in openbare gelegenheden.</li>
+            <li>Strafbare feiten opsporen.</li>
+            <li>Hulp verlenen als er iemand in nood is.</li>
+            <li>Overleg met de gemeente, het Openbaar Ministerie en partners over veiligheidsproblemen.</li>
+            <li>Het uitvoeren van politietaken voor justitie.</li>
+        </ul>
+    </div>
+
+    <div class="line_black">
+    </div>
+
+    <h1 style="text-align: center;">Onze korpsleiding</h1>
+    <div class="korpsleiding_container">
+        <div class="korpschef">
+            <img src="./resources/person_template.png" class="korpsleiding_icons">
+            <h2>Nick Boos</h2>
+            <p>Korpschef</p>
+        </div>
+    </div>
+    <div class="korpsleiding_row">
+        <div class="korpsleiding_left_row">
+            <div class="korpleiding">
+                <img src="./resources/person_template.png" class="korpsleiding_icons">
+                <h2>Sander Dekkers</h2>
+                <p>Hoofdcommissaris</p>
+            </div>
+        </div>
+        <div class="korpsleiding_middle_row">
+        <div class="korpleiding">
+                <img src="./resources/person_template.png" class="korpsleiding_icons">
+                <h2>Tommy Huisbaas</h2>
+                <p>Hoofdcommissaris</p>
+            </div>
+        </div>
+        <div class="korpsleiding_right_row">
+            <div class="korpleiding">
+                <img src="./resources/person_template.png" class="korpsleiding_icons">
+                <h2>Florian Huisbaas</h2>
+                <p>Hoofdcommissaris</p>
+            </div>
+        </div>
+    </div>
+    <div class="korpsleiding_row">
+        <div class="korpsleiding_left_row">
+            <div class="korpleiding">
+                <img src="./resources/person_template.png" class="korpsleiding_icons">
+                <h2>Sander Dekkers</h2>
+                <p>Hoofdcommissaris</p>
+            </div>
+        </div>
+        <div class="korpsleiding_middle_row">
+        <div class="korpleiding">
+                <img src="./resources/person_template.png" class="korpsleiding_icons">
+                <h2>Tommy Huisbaas</h2>
+                <p>Hoofdcommissaris</p>
+            </div>
+        </div>
+        <div class="korpsleiding_right_row">
+            <div class="korpleiding">
+                <img src="./resources/person_template.png" class="korpsleiding_icons">
+                <h2>Florian Huisbaas</h2>
+                <p>Hoofdcommissaris</p>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-2" style="background-color: white;">
+            ©Copyright: <a class="text-dark" href="https://tedeapolis.nl/">Tedeapolis Politie</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
 
     <script src="./lib/jquery/dist/jquery.min.js"></script>
     <script src="./lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./js/site.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    
 </body>
 
 </html>
